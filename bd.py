a43b1cee0fa6618999d40414c03b4ccbe57ad7fa
yohannNDB <91455596+yohannNDB@users.noreply.github.com>
2022-09-30 09:57:03 +0200

    Update bd.py

--- bd.py
+++ bd.py
@@ -1,5 +1,5 @@
-# Créé par merle, le 23/09/2022 en Python 3.7
-class Caractéristiques():
+# Cree par merle, le 23/09/2022 en Python 3.7
+class Caracteristiques():
     def __init__(self,genre,age,caractere, music_genre, astro, orientation, origine):
         self.age = age
         self.genre=genre
@@ -34,47 +34,60 @@ class Profil():
             return print(nouvelle_orientation)
 
 
-pretendant = [Jean_Kule, Magg_Rosbeat, Fany_Epi, Faustine_MuletLaLibido, Erwan_Erforsouhone, Eugène_Nocide, Eva_Nouie, Frank_NStein, ElsaRose_LaPlante, Laurent_Outan, Scott_Che, Jessica_Pote]
-someone = random(pretendant)
-
-Jean_Kule= Caractéristiques("18","homme","romantic","rap","cancer","gay","americain")
-
-Magg_Rosbeat = Caractéristiques("87","femme","agressif","rock","verge","lesbienne","americain")
-
-Fany_Epi = Caractéristiques("45","femme","depressif","rap","gemaux","hetero","marocain")
-
-Faustine_MuletLaLibido= Caractéristiques("25","femme","fetard","pop","poisson","hetero","espagnol")
-
-Erwan_Erforsouhone= Caractéristiques("30","homme","passif","rock","capricorne","hetero","marocain")
-
-Eugène_Nocide= Caractéristiques("24","homme","agressif","électro","verge","hetero", "francais")
-
-Eva_Nouie = Caractéristiques("46","femme","romantic","pop","poisson","hetero","français")
-
-Frank_NStein = Caractéristiques("86","homme","agressif","rock","gemaux","gay","américain")
-
-ElsaRose_LaPlante = Caractéristiques("22","femme","depressif","rap","capricorne","lesbienne","espagnol")
-
-Laurent_Outan = Caractéristiques("35","homme","fetard","pop","poisson","hetero","francais")
-
-Scott_Che = Caractéristiques("26","homme","romantic","rap","cancer","hetero","americain")
-
-Jessica_Pote = Caractéristiques("19","femme","fetard","rock","gemaux","hetero","espagnol")
 
 
+Jean_Kule = Caracteristiques\
+            ("18","homme","romantic","rap","cancer","gay","americain")
+
+Magg_Rosbeat = Caracteristiques\
+            ("87","femme","agressif","rock","verge","lesbienne","americain")
+
+Fany_Epi = Caracteristiques\
+            ("45","femme","depressif","rap","gemaux","hetero","marocain")
+
+Faustine_MuletLaLibido= Caracteristiques\
+            ("25","femme","fetard","pop","poisson","hetero","espagnol")
+
+Erwan_Erforsouhone= Caracteristiques\
+            ("30","homme","passif","rock","capricorne","hetero","marocain")
+
+Eugène_Nocide= Caracteristiques\
+            ("24","homme","agressif","electro","verge","hetero", "francais")
+
+Eva_Nouie = Caracteristiques\
+            ("46","femme","romantic","pop","poisson","hetero","français")
+
+Frank_NStein = Caracteristiques\
+            ("86","homme","agressif","rock","gemaux","gay","americain")
+
+ElsaRose_LaPlante = Caracteristiques\
+            ("22","femme","depressif","rap","capricorne","lesbienne","espagnol")
+
+Laurent_Outan = Caracteristiques\
+            ("35","homme","fetard","pop","poisson","hetero","francais")
+
+Scott_Che = Caracteristiques\
+            ("26","homme","romantic","rap","cancer","hetero","americain")
+
+Jessica_Pote = Caracteristiques\
+            ("19","femme","fetard","rock","gemaux","hetero","espagnol")
 
 
 
-Nom_prenom = input("Entrer votre nom et votre prénom: ")
+
+
+
+Nom_prenom = input("Entrer votre nom et votre prenom: ")
 Nom_prenom = Profil(\
              input("Entrer votre âge: "),\
 
-             input("Entrer votre genre h/f: "),\
+             input("Entrer votre genre\
+                    homme/femme: "),\
 
              input("Entrer votre caractère\
                     romantic/agressif/depressif/fetard/passif: "),\
 
-             input("Entrer votre style de musique préféré:\
+             input("Entrer votre style de musique prefere:\
                     rap/pop/electro/rock: "),\
 
              input("Entrer votre signe astrologique\
@@ -83,11 +96,17 @@ Nom_prenom = Profil(\
              input("Entrer votre orientation sexuelle\
                     hetero/lesbienne/gay: "),\
 
-             input("Entrer votre nationalité\
+             input("Entrer votre nationalite\
                     espagnol/americain/francais/marocain/autre: ")
              )
+def dist_hamming(m1,m2):
+    d = abs(len(m1)-len(m2))
+    for a,b in zip(m1,m2):
+        if a != b :
+            d += 1
+    return d
 
-def distance_hamming_rec_cache_insecable(m1,m2,cache=None):
+def distance_hamming_rec(m1,m2,cache=None):
     if cache is None:
         cache = {}
     if (m1,m2) in cache:
@@ -98,9 +117,9 @@ def distance_hamming_rec_cache_insecable(m1,m2,cache=None):
     else:
         i = len(m1)
         j = len(m2)
-        d1 = distance_edition_rec_cache_insecable(m1[:i-2],m2[:j-1], cache) + dist_hamming(m1[i-2:], m2[j-1:])
-        d2 = distance_edition_rec_cache_insecable(m1[:i-1],m2[:j-2], cache) + dist_hamming(m1[i-1:], m2[j-2:])
-        d3 = distance_edition_rec_cache_insecable(m1[:i-1],m2[:j-1], cache) + dist_hamming(m1[i-1:], m2[j-1:])
+        d1 = distance_hamming_rec(m1[:i-2],m2[:j-1], cache) + dist_hamming(m1[i-2:], m2[j-1:])
+        d2 = distance_hamming_rec(m1[:i-1],m2[:j-2], cache) + dist_hamming(m1[i-1:], m2[j-2:])
+        d3 = distance_hamming_rec(m1[:i-1],m2[:j-1], cache) + dist_hamming(m1[i-1:], m2[j-1:])
         cache[m1,m2] = min(d1,d2,d3)
         return cache[m1,m2]
 
@@ -112,57 +131,68 @@ not_possible_match = []
 
 
 
-def check_for_match(self,Nom_prenom,someone):
-    if self.Age < 28 and self.age < 28:
-        distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None)
-        print(distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None))
-        if distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None) == 2:
-            pass
-        else:
-            distance_hamming_rec_cache_insecable(self.Orientation_sex,self.orientation_sex,cache=None)
-            if distance_hamming_rec_cache_insecable(self.Orientation_sex,self.orientation_sex,cache=None) == 0:
-                if distance_hamming_rec_cache_insecable(self.Origine,self.origine,cache=None) == 0:
-                    return True
-                elif distance_hamming_rec_cache_insecable(self.Origine,self.origine,cache=None) == 1:
-                    pass
+def check_for_match(Nom_prenom,someone):
+    A = Nom_prenom.genre
+    a = someone.genre
+    B = Nom_prenom.orientation_sex
+    b = someone.orientation_sex
+    C = Nom_prenom.origine
+    c = someone.origine
+    if personnage !=[]:
+
+        #Verifie si meme tranche d'age
+        if Nom_prenom.Age < 28 and someone.age < 28:
+            #distance_hamming_rec(A,a,cache=None)
+            #Si l'écart entre variable A et a ==2 cest donc un homme et une femme
+            if distance_hamming_rec(A,a,cache=None) == 2:
+                if distance_hamming_rec(B,b) == 0:
+                    if distance_hamming_rec(C,c) > 5:
+                        possible_match.append(someone)
+                    else:
+                        not_possible_match.append(someone)
 
             else:
-                pretendant.remove(someone)
-                return False
+                #distance_hamming_rec(self.Orientation_sex,self.orientation_sex,cache=None)
+                if distance_hamming_rec(A,a,cache=None) == 0:
+                    if distance_hamming_rec(self.Orientation_sex,self.orientation_sex,cache=None) == 0:
+                        if distance_hamming_rec(self.Origine,self.origine,cache=None) == 0:
+                            return True
+                        elif distance_hamming_rec(self.Origine,self.origine,cache=None) == 1:
+                            pass
+
+                    else:
+                        pretendant.remove(someone)
+                        return False
 
 
-    if self.Age > 28 and self.Age < 38 and self.age >28 and self.age < 38:
-            distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None)
-            print(distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None))
-            if distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None) == 2:
-                pass
+        elif self.Age > 28 and self.Age < 38 and self.age >28 and self.age < 38:
+            distance_hamming_rec(self.Genre,self.genre,cache=None)
+            print(distance_hamming_rec(self.Genre,self.genre,cache=None))
+            if distance_hamming_rec(self.Genre,self.genre,cache=None) == 2:
+                distance_hamming_rec(self.Genre,self.genre,cache=None)
             else:
-                distance_hamming_rec_cache_insecable(self.Orientation_sex,self.orientation_sex,cache=None)
-                if distance_hamming_rec_cache_insecable(self.Orientation_sex,self.orientation_sex,cache=None) == 0:
-                    if distance_hamming_rec_cache_insecable(self.Origine,self.origine,cache=None) == 0:
-                        return True
-                    elif distance_hamming_rec_cache_insecable(self.Origine,self.origine,cache=None) == 1:
+                distance_hamming_rec(self.Orientation_sex,self.orientation_sex,cache=None)
+                if distance_hamming_rec(self.Orientation_sex,self.orientation_sex,cache=None) == 0:
+                    if distance_hamming_rec(self.Origine,self.origine,cache=None) == 0:
+                        possible_match.append(someone)
+                    elif distance_hamming_rec(self.Origine,self.origine,cache=None) == 1:
                         pass
-
                 else:
                     pretendant.remove(someone)
-                    return False
 
-check_for_match()
-
-
-
-
-
-
-def match(Nom_prenom):
-    check_for_match(Nom_prenom,someone)
-    if check_for_match == True:
-        possible_match.append(someone)
-        propose_match_to_profile()
+        else :
+            pass
     else:
-        not_possible_match.append(someone)
+        print("Les personnes pouvant match avec vous sont: " +str(possible_match))
         return
+
+
+distance_hamming_rec_cache_insecable(self.Genre,self.genre,cache=None)
+
+
+
+def match(possible_match):
+
 
 
 
